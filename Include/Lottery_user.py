45e99a2d93213eb904177b1e9207925f34ea67dc
Bruno D. Jesus <72465456+brunobdj@users.noreply.github.com>
2021-08-03 21:29:18 -0300

    Create Lottery_user.py

---
+++ Include/Lottery_user.py
@@ -0,0 +1,16 @@
+# User Lottery's
+import requests 
+
+resp = requests.get('https://randomuser.me/api/?results=100')
+
+data = resp.json()
+
+perfil = data['results'][0]
+
+###afim de avaliar as informações presentes no json, sobre o perfil de cada usuario
+
+for user in data['results'][:1]:
+    print(user['name']['first'],':', '\n')
+    for ctg in perfil:
+        print('\n','-',ctg,'\n')
+
